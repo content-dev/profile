324d59af3f5a1396d60a34f2c859105449115fba
Armando Hernandez <armand116@hotmail.com>
2022-04-13 18:21:08 -0500

    Updates

---
+++ precache-manifest.7d8644ca691c7ff9bbecda796301506b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c86ff7dfc9c94f60894e83f4a033ac8",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "a8e4c10b816fbb62f941",
+    "url": "/profile/static/css/main.e0b78bfd.chunk.css"
+  },
+  {
+    "revision": "36515e8ddeccadd7ac8e",
+    "url": "/profile/static/js/2.9b192fe9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/profile/static/js/2.9b192fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e4c10b816fbb62f941",
+    "url": "/profile/static/js/main.0bd56010.chunk.js"
+  },
+  {
+    "revision": "5076af70b647b2393d8e",
+    "url": "/profile/static/js/runtime-main.8771bb24.js"
+  },
+  {
+    "revision": "e0e04d06c1898b6f39c9d80c6633b3df",
+    "url": "/profile/static/media/armando-hdz-01.e0e04d06.png"
+  },
+  {
+    "revision": "3b847c2bed2875145c675c569e8661d7",
+    "url": "/profile/static/media/ibm-01.3b847c2b.png"
+  },
+  {
+    "revision": "a5fd16c50bc164c69bf06b885cbee444",
+    "url": "/profile/static/media/itexico-01.a5fd16c5.png"
+  },
+  {
+    "revision": "137b26c814e2ba00c13a58c636a48dd5",
+    "url": "/profile/static/media/tcs-01.137b26c8.png"
+  },
+  {
+    "revision": "cbc2d5015b8251743a911131021604c7",
+    "url": "/profile/static/media/video-01.cbc2d501.png"
+  },
+  {
+    "revision": "f8b144fb6e2d049c8d2835a4c00e1e7e",
+    "url": "/profile/static/media/video-02.f8b144fb.png"
+  },
+  {
+    "revision": "2af6b9a2515743c245cb996343a8fb26",
+    "url": "/profile/static/media/video-03.2af6b9a2.png"
+  },
+  {
+    "revision": "6c42ce77af612b6310eb7748541f6d5e",
+    "url": "/profile/static/media/video-04.6c42ce77.png"
+  },
+  {
+    "revision": "2b3f256bc0295f1647cc7be896b1889c",
+    "url": "/profile/static/media/video-05.2b3f256b.png"
+  },
+  {
+    "revision": "bae92a1d9db95726b89b1bf1beebee1c",
+    "url": "/profile/static/media/video-06.bae92a1d.png"
+  }
+]);
